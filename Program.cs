db69b3b4bbbdab4f34a5c9805363944e5536c6f9
Dhenao911 <dhenao911@gmail.com>
2025-03-08 11:54:26 -0500

    Actualización de Program.cs: configuración de servicios y Swagger

--- Program.cs
+++ Program.cs
@@ -1,18 +1,74 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using csharpapi.Services;
+using Microsoft.OpenApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ControlConexion>();
+builder.Services.AddSingleton<TokenService>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Api Genérica C#",
+        Version = "v1",
+        Description = "API de prueba con ASP.NET Core y Swagger",
+        Contact = new OpenApiContact
+        {
+            Name = "Soporte API",
+            Email = "soporte@miapi.com",
+            Url = new Uri("https://miapi.com/contacto")
+        }
+    });
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Genérica C#");
+        c.RoutePrefix = "swagger";
+    });
 }
 
+
 app.UseHttpsRedirection();
+app.UseCors("AllowAllOrigins");
+app.UseSession();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
 
 var summaries = new[]
 {
